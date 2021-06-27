84151160972588c0cb79ef7a01c6f9a01f20f88d
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-06-27 17:41:45 -0300

    até botão de descansar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
-  return <PomodoroTimer defaultPomodoroTime={3600} />;
+  return (
+    <div className="container">
+      <PomodoroTimer
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,15 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useInterval from '../hooks/use-interval';
-import { secondsToTime } from '../utils/seconds-to-time';
+import { Button } from './Button';
+import { Timer } from './timer';
 
 interface Props {
-  defaultPomodoroTime: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
-  return <div>Olá mundo {secondsToTime(mainTime)}</div>;
+  const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  useEffect(() => {
+    if (working) {
+      document.body.classList.add('working');
+    }
+    if (resting) {
+      document.body.classList.remove('working');
+    }
+  }, [working, resting]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+  const configRest = (long: boolean) => {
+    setTimeCounting(true);
+    setWorking(false);
+    setResting(true);
+    if (long) {
+      setMainTime(props.longRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
+  };
+  return (
+    <div className="pomodoro">
+      <h2>You are: working</h2>
+      <Timer mainTime={mainTime} />
+      <div className="controls">
+        <Button
+          text="Work"
+          onClick={() => {
+            setTimeCounting(true);
+            setWorking(true);
+            setResting(false);
+            setMainTime(props.pomodoroTime);
+          }}
+        />
+        <Button text="Rest" onClick={() => configRest(false)} />
+        {!working && !resting ? null : (
+          <Button
+            text={timeCounting ? 'pause' : 'Play'}
+            onClick={() => {
+              setTimeCounting(!timeCounting);
+            }}
+          />
+        )}
+      </div>
+      <div className="details">
+        <p> dddddd</p>
+      </div>
+    </div>
+  );
 }
