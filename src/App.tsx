d0d632585d8e9c18da0b132c8059064a0527090f
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-06-27 13:53:42 -0300

    Até a função deconverter

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
+
+function App(): JSX.Element {
+  return <PomodoroTimer defaultPomodoroTime={3600} />;
+}
+
+export default App;
